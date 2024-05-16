cec7fb6ad749adf147f711f38a0e9d9c6fecab83
0xb10c <b10c@b10c.me>
2024-05-16 16:04:34 +0200

    add: ckpool module (not for actual pool usage)

--- modules/default.nix
+++ modules/default.nix
@@ -1,5 +1,6 @@
 {
   addrman-observer = ./addrman-observer;
+  ckpool = ./ckpool;
   fork-observer = ./fork-observer;
   github-metadata-backup = ./github-metadata-backup;
   github-metadata-mirror = ./github-metadata-mirror;
