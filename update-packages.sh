e94b2d9575309062dd2e75c7b5e9396e051fe2ca
0xb10c <b10c@b10c.me>
2024-09-26 00:37:42 +0200

    add: workflow to auto-update packages

--- update-packages.sh
+++ update-packages.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 declare -a pkgs=(
-  # "addrman-observer"
-  # "fork-observer"
-  # "miningpool-observer"
-  # "peer-observer"
-  # "github-metadata-mirror"
-  # "github-metadata-backup"
+  "addrman-observer"
+  "fork-observer"
+  "miningpool-observer"
+  "peer-observer"
+  "github-metadata-mirror"
+  "github-metadata-backup"
   "transactionfee-info-backend"
 )
 
 for i in "${pkgs[@]}"
 do
   echo "updating $i"
-  nix-update --flake --version=skip $i
+  nix-shell -p nix-update --run "nix-update --flake --version=branch $i"
 done
