6f74d1bbc7c1edd0f56433be4ed3030fc21d8447
0xb10c <b10c@b10c.me>
2024-10-22 14:04:05 +0200

    add: miningpool-observer module

--- modules/default.nix
+++ modules/default.nix
@@ -4,6 +4,7 @@
   fork-observer = ./fork-observer;
   github-metadata-backup = ./github-metadata-backup;
   github-metadata-mirror = ./github-metadata-mirror;
+  miningpool-observer = ./miningpool-observer;
   peer-observer = ./peer-observer;
   stratum-observer = ./stratum-observer;
   transactionfee-info = ./transactionfee-info;
