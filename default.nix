62e75d25420cf4e09cbca4f6b4fd1a36d63d3451
0xb10c <b10c@b10c.me>
2024-10-31 17:53:09 +0100

    add: bitcoind tracing 27.2 and 28.0

--- default.nix
+++ default.nix
@@ -6,7 +6,8 @@
   bitcoind-tracing-v24 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v24.2"; };
   bitcoind-tracing-v25 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v25.2"; };
   bitcoind-tracing-v26 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v26.2"; };
-  bitcoind-tracing-v27 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v27.1"; };
+  bitcoind-tracing-v27 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v27.2"; };
+  bitcoind-tracing-v28 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v28.0"; };
   ckpool = pkgs.callPackage ./pkgs/ckpool { };
   fork-observer = pkgs.callPackage ./pkgs/fork-observer { };
   github-metadata-backup = pkgs.callPackage ./pkgs/github-metadata-backup { };
