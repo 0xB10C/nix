cbdabef3f0c19779adf0d03493e6404b0992b6d4
0xb10c <b10c@b10c.me>
2024-10-31 14:38:57 +0100

    github-metadata-mirror: 75c94609904694909919a0df70afd50a6f2fed36 -> 0-unstable-2024-09-27
    Diff: https://github.com/0xB10C/github-metadata-mirror/compare/75c94609904694909919a0df70afd50a6f2fed36...80b199bb329c7d5ee9b8ce90400e0f35ec83cc63

--- pkgs/github-metadata-mirror/default.nix
+++ pkgs/github-metadata-mirror/default.nix
@@ -2,13 +2,13 @@
 
 stdenv.mkDerivation rec {
   name = "github-metadata-mirror";
-  version = "75c94609904694909919a0df70afd50a6f2fed36";
+  version = "80b199bb329c7d5ee9b8ce90400e0f35ec83cc63";
   
   src = pkgs.fetchFromGitHub {
     owner = "0xB10C";
     repo = "github-metadata-mirror";
     rev = version;
-    sha256 = "sha256-aIbY2x2fd2s7hPf4cT8B7UbERuVPMWmp9EF0Wlry/m0=";
+    sha256 = "sha256-z1wPUKYDtkjEsan1sNbwNkyiG6IMkrLbObnObLERPCY=";
   };
 
   installPhase = ''
