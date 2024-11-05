a9f921e97806f7cecb9fb23acef2a09876aaf236
0xb10c <b10c@b10c.me>
2024-11-05 11:19:30 +0100

    update: update-packages.sh to match CI

--- update-packages.sh
+++ update-packages.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
-declare -a pkgs=(
-  "addrman-observer"
-  "fork-observer"
-  "miningpool-observer"
-  "peer-observer"
-  "github-metadata-mirror"
-  "github-metadata-backup"
-  "transactionfee-info-backend"
-)
-
+pkgs=($(nix flake show --json | jq '.packages."x86_64-linux" | keys[] ' | grep -v bitcoind-tracing))
 for i in "${pkgs[@]}"
 do
-  echo "updating $i"
-  nix-shell -p nix-update --run "nix-update --flake --commit --version=branch $i"
-  git log -1
+  echo "trying to update package: '$i'"
+  nix-shell -p nix-update --run "nix-update --flake --commit --build --version=branch $i"
+  git restore pkgs/
 done
+git log -1
