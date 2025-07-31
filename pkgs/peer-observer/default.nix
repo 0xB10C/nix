967074ec95eee03152258f7944981041d4dd0659
0xb10c <b10c@b10c.me>
2025-07-31 18:45:03 +0200

    update: peer-observer package with rpc-extractor in it

--- pkgs/peer-observer/default.nix
+++ pkgs/peer-observer/default.nix
@@ -2,13 +2,13 @@
 
 rustPlatform.buildRustPackage rec {
   name = "peer-observer";
-  version = "9b2453e9915cc4942816d85c92fd852ebb2597ee";
+  version = "0d1b10ccffabc64a722eb202389425c5bfbf8eec";
 
   src = pkgs.fetchFromGitHub {
     owner = "0xB10C";
     repo = "peer-observer";
     rev = version;
-    sha256 = "sha256-0QbA4WTjVJkSRb0iPPaFZedu78MyK+VmOaCEuta9fJw=";
+    sha256 = "sha256-Ya990Mvx7DGMWGycpjBDd58Ajuc16aXPugExYeZ5wXg=";
   };
 
   hardeningDisable = [
@@ -34,7 +34,7 @@ rustPlatform.buildRustPackage rec {
     pkgs.rustfmt
   ];
 
-  cargoHash = "sha256-8cA6BONuHUIq4D8sUMfqdv+HNpoQxRFsrbfKZhuKIG8=";
+  cargoHash = "sha256-OFzuH3y4XuGu+7QdbY1hEzdTublavTSDMHGA3LWarG4=";
 
   meta = with stdenv.lib; {
     description = "Hooks into Bitcoin Core to observe how our peers interact with us.";
