761e26c84a194535d43987ff2b6fd43003ec595f
0xb10c <b10c@b10c.me>
2025-07-31 15:09:46 +0200

    add: bitcoind v29 and use it in peer-observer test
    (the switch from autotools to cmake happened between v28 and v29)

--- pkgs/bitcoind-tracing/default.nix
+++ pkgs/bitcoind-tracing/default.nix
@@ -1,11 +1,14 @@
 { stdenv
+, lib
 , fetchFromGitHub
 , pkg-config
 , autoreconfHook
+, cmake
 , boost
 , libevent
 , libsystemtap
 , version
+, useCmake ? false
 , ...
 }:
 
@@ -27,13 +30,30 @@ stdenv.mkDerivation rec {
       "v27.1" = "sha256-1Gpesss4MTiU3Omi/scDV0iU2de4j/oDIY3Y5H2KQp4=";
       "v27.2" = "sha256-E0+3tON+uIkqN+QEmZZSEfDCDcCdYtJjfhcDFYs1zH4=";
       "v28.0" = "sha256-LLtw6pMyqIJ3IWHiK4P3XoifLojB9yMNMo+MGNFGuRY=";
+      "v29.0" = "sha256-XvoqYA5RYXbOjeidxV4Wxb8DhYv6Hz510XNMhmWkV1Y=";
     }.${version} or (builtins.trace "Bitcoin Core using dummy vendor SHA256" "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=");
   };
 
-  nativeBuildInputs = [ pkg-config autoreconfHook libsystemtap ];
+  nativeBuildInputs = [ 
+    pkg-config
+    (if useCmake then cmake else autoreconfHook)
+    libsystemtap
+  ];
   buildInputs = [ boost libevent libsystemtap ];
 
-  configureFlags = [
+  cmakeFlags = if useCmake then [
+    (lib.cmakeBool "BUILD_BENCH" false)
+    (lib.cmakeBool "WITH_ZMQ" false)
+    (lib.cmakeBool "WITH_BDB" false)
+    (lib.cmakeBool "WITH_USDT" true)
+    (lib.cmakeBool "BUILD_TESTS" false)
+    (lib.cmakeBool "BUILD_FUZZ_BINARY" false)
+    (lib.cmakeBool "BUILD_GUI_TESTS" false)
+    (lib.cmakeBool "ENABLE_WALLET" false)
+    (lib.cmakeBool "BUILD_GUI" false)
+  ] else [];
+
+  configureFlags = if useCmake then [] else [
     "--with-boost-libdir=${boost.out}/lib"
     "--disable-shared"
     "--disable-wallet"
