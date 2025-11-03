0d2d592b3c73f78e019e30532824622c889cd913
0xb10c <b10c@b10c.me>
2025-11-03 21:06:49 +0100

    update: github-metadata-backup
    to include https://github.com/0xB10C/github-metadata-backup/pull/10

--- pkgs/github-metadata-backup/default.nix
+++ pkgs/github-metadata-backup/default.nix
@@ -2,16 +2,16 @@
 
 pkgs.rustPlatform.buildRustPackage rec {
   name = "github-metadata-backup";
-  version = "7bea8fe080920df00751c7601dd95205debbabad";
-  
+  version = "b5adba1de2794a1a7c362ad927b8b30502aac3bf";
+
   src = pkgs.fetchFromGitHub {
     owner = "0xb10c";
     repo = "github-metadata-backup";
     rev = version;
-    sha256 = "sha256-p/pKlpIwIgNZ4iAC6S3II83eQPPxbmKdPe5p4emo0Dg=";
+    sha256 = "sha256-8aoK1pbqLc5BiJrW6kvdyjNue5I74okrnpIp1uIrQi4=";
   };
 
-  cargoHash = "sha256-QZDu/fDKkAq7NsnGWOeSjDs3IedibslcLkZTujPaGdE=";
+  cargoHash = "sha256-JHW6Gfs4axnYC8BMcV8nGOqTnWyln669kS4SpVPFLFw=";
 
   useFetchCargoVendor = true;
 
