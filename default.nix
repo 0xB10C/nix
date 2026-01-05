c14d14645201e3300c07c2f200f810603bda4992
0xb10c <b10c@b10c.me>
2026-01-05 21:51:14 +0100

    remove: Bitcoin Core v23, v24, v25, v26, and v27
    The build is broken in CI on nixos-unstable, it's out-dated and it's
    not used anymore.

--- default.nix
+++ default.nix
@@ -3,11 +3,6 @@
 {
   addrman-observer = pkgs.callPackage ./pkgs/addrman-observer { };
   asmap-data = pkgs.callPackage ./pkgs/asmap-data { };
-  bitcoind-tracing-v23 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v23.2"; };
-  bitcoind-tracing-v24 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v24.2"; };
-  bitcoind-tracing-v25 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v25.2"; };
-  bitcoind-tracing-v26 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v26.2"; };
-  bitcoind-tracing-v27 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v27.2"; };
   bitcoind-tracing-v28 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v28.0"; };
   bitcoind-tracing-v29 = pkgs.callPackage ./pkgs/bitcoind-tracing { version = "v29.0"; useCmake = true; };
   ckpool = pkgs.callPackage ./pkgs/ckpool { };
--- pkgs/bitcoind-tracing/default.nix
+++ pkgs/bitcoind-tracing/default.nix
@@ -23,18 +23,12 @@ stdenv.mkDerivation rec {
     repo = "bitcoin";
     rev = version;
     sha256 = {
-      "v23.2" = "sha256-iE247MKKzY5yB7ihbVJ9sBtciFJ84bSZhd4URthrJqM=";
-      "v24.2" = "sha256-ndr1JqwERKBQzKB0xgIOUC2+bF43IlajCAnIieZBNr0=";
-      "v25.2" = "sha256-BzND0AE7Xhf/pX2jedoPIHk3J77IwOpZ2WOzaYiZyzE=";
-      "v26.2" = "sha256-hXE9FVb4Fnz0DofQfkVp08p15qvhdGc6+eQ6P4IJVhE=";
-      "v27.1" = "sha256-1Gpesss4MTiU3Omi/scDV0iU2de4j/oDIY3Y5H2KQp4=";
-      "v27.2" = "sha256-E0+3tON+uIkqN+QEmZZSEfDCDcCdYtJjfhcDFYs1zH4=";
       "v28.0" = "sha256-LLtw6pMyqIJ3IWHiK4P3XoifLojB9yMNMo+MGNFGuRY=";
       "v29.0" = "sha256-XvoqYA5RYXbOjeidxV4Wxb8DhYv6Hz510XNMhmWkV1Y=";
     }.${version} or (builtins.trace "Bitcoin Core using dummy vendor SHA256" "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=");
   };
 
-  nativeBuildInputs = [ 
+  nativeBuildInputs = [
     pkg-config
     (if useCmake then cmake else autoreconfHook)
     libsystemtap
@@ -62,7 +56,7 @@ stdenv.mkDerivation rec {
     "--enable-fuzz-binary=no"
     "--enable-ebpf"
   ];
-  
+
   doCheck = false;
   enableParallelBuilding = true;
 }
