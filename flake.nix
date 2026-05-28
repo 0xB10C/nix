6a1c29a2c48594a0a09d5e358699d6ada225dcaf
0xB10C <b10c@b10c.me>
2026-05-28 09:53:42 +0200

    upgrade: NixOS 26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "My personal NUR repository";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
 
   outputs =
     { self, nixpkgs, ... }:
